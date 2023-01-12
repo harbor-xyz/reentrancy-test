b32b60fa7c8cdaea4a7a33cc576d201e7c3a85dc
cherrypopsicle <majd.khoury93@gmail.com>
2023-01-12 15:33:35 +0400

    updated

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # Reentrancy Testnet
 
 This is a sample project that shows a basic reentrancy attack Testnet.
+
+## Installation
+
+To install the packages, run either the following in the root directory of your repository:
+
+```
+yarn install
+
+```
+
+OR
+
+```
+npm install
+
+```
+
+## Run the tests
+
+After setting up your tests, run:
+
+```
+yarn test
+```
+
+To set up your tests. Make sure to fill in the credentials in the test files!
+
+```javascript
+harbor = new Harbor({
+  userKey: "cFeJWnDwQFVTSF2AabM2W5",
+  projectKey: "fPMeKGPUfyBTCoq2omv3G4",
+});
+```
--- test/protected-re-entrancy-solution.test.js
+++ test/protected-re-entrancy-solution.test.js
@@ -1,4 +1,4 @@
-const Harbor = require("@beam-me-up/harbor");
+const Harbor = require("@harbor-xyz/harbor");
 const hre = require("hardhat");
 const { ethers } = require("ethers");
 const chai = require("chai");
@@ -9,6 +9,11 @@ function generateRandomTestnetName() {
   return `testnet-${Math.floor(Math.random() * 1000)}`;
 }
 
+/**
+ * Note: running this solution will NOT work unless you have filled the Harbor
+ * Credentials, modify ProtectedBank as per the document's instructions and compile
+ * the contracts `npx hardhat compile`.
+ */
 describe("Re-entrancy test", () => {
   let harbor;
   let testnet;
@@ -23,10 +28,11 @@ describe("Re-entrancy test", () => {
 
   const testnetName = generateRandomTestnetName();
 
+  // Fill in your credentials here!
   beforeAll(async () => {
     harbor = new Harbor({
-      userKey: "cFeJWnDwQFVTSF2AabJmW5",
-      projectKey: "fPMeKGPUfyBTCoqtXmv3G4",
+      userKey: "9S7NYNRjgy6Xaw5eSdaGqg",
+      projectKey: "sJHqS5q4B2jb6TwDP6pHm5",
     });
 
     await harbor.authenticate();
@@ -47,7 +53,6 @@ describe("Re-entrancy test", () => {
       },
       testnetName
     );
-
     signers = await hre.ethers.getSigners();
     chains = await testnet.chains();
     ethereum = chains[0];
@@ -75,7 +80,6 @@ describe("Re-entrancy test", () => {
   }, 50000);
   it("Deposits 10 ETH into the bank vault from 3 different users", async () => {
     const tenEthers = ethers.utils.parseEther("10");
-    let totalFunds = 0;
     for (let i = 0; i < 3; i++) {
       protectedBankContract = new ethers.Contract(
         bankInfo.address,
@@ -87,6 +91,19 @@ describe("Re-entrancy test", () => {
     const balance = (await protectedBankContract.vault()).toString();
     const balanceFormatted = Number(balance) / 1e18;
     expect(balanceFormatted).to.eql(30);
+
+    // Using the SDK to check ProtectedBank balance
+    const chains = await testnet.chains();
+    const accounts = await chains[0].accounts();
+    for (let i = 0; i < accounts.length; i++) {
+      if (accounts[i].type == "contract") {
+        if (accounts[i].name == "ProtectedBank") {
+          const balance = accounts[i].balances[0].amount;
+          const balanceFormatted = Number(balance) / 1e18;
+          expect(balanceFormatted).to.eql(30);
+        }
+      }
+    }
   }, 50000);
   it("Attempting to attack the ProtectedBank will reject!", async () => {
     const oneEther = ethers.utils.parseEther("1");
@@ -98,5 +115,18 @@ describe("Re-entrancy test", () => {
     const balance = (await protectedBankContract.vault()).toString();
     const balanceFormatted = Number(balance) / 1e18;
     expect(balanceFormatted).to.eql(30);
-  });
+
+    // Using the SDK to check ProtectedBank balance
+    const chains = await testnet.chains();
+    const accounts = await chains[0].accounts();
+    for (let i = 0; i < accounts.length; i++) {
+      if (accounts[i].type == "contract") {
+        if (accounts[i].name == "ProtectedBank") {
+          const balance = accounts[i].balances[0].amount;
+          const balanceFormatted = Number(balance) / 1e18;
+          expect(balanceFormatted).to.eql(30);
+        }
+      }
+    }
+  }, 50000);
 });
